97d956f6ff56b9f8711ce2d3217948f6f6c136db
任跃兵 <cnryb@live.cn>
2017-02-21 22:49:07 +0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SendMail4VSTS
-send mail extensions for Visual Studio Team Services
+#SendMail4VSTS
+
+###This is an extension for Visual Studio Team Services.
+
+This extension takes care of sending email within your build or release pipeline for Visual Studio Team Services.
+
+It can run on different platforms, for example Mac Os, Windows or Linux.
+
+
+
---
+++ SendMail/sendMail.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var os = require('os');
+var tl = require('vsts-task-lib/task');
+var nodemailer = require('nodemailer');
+
+
+try {
+    tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+    var username = tl.getInput('SmtpUsername', false);
+    var password = tl.getInput('SmtpPassword', false);
+
+    var server = tl.getInput('SmtpServer', true);
+    var isSSL = tl.getBoolInput('UseSSL', false);
+    var port = tl.getInput('SmtpPort', true);
+
+    var subject = tl.getInput("Subject", true);
+    var body = tl.getInput("Body", true);
+    var to = tl.getInput("To", true);
+    var from = tl.getInput("From", true);
+
+    var attachmentPath = tl.getInput("Attachment", false);
+    tl.debug("get all");
+
+    var stransporter = nodemailer.createTransport({
+        host: server,
+        secure: isSSL, // use SSL
+        port: port, // port
+        auth: {
+            user: username,
+            pass: password
+        }
+    });
+
+    function ssl() {
+        var mailOptions = {
+            from: from,
+            to: to,
+            subject: subject,
+            html: body
+        }
+        if (attachmentPath) {
+            mailOptions.attachments =   [{
+                path: attachmentPath
+            }];
+        }
+        return mailOptions;
+    }
+    tl.debug("ready");
+    stransporter.sendMail(ssl(), function (error, info) {
+        if (error) {
+            console.log(error);
+            tl.error(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+            tl.setResult(tl.TaskResult.Succeeded);
+        }
+    });
+}
+catch (err) {
+    tl.error(err.message);
+    tl.setResult(tl.TaskResult.Failed, tl.loc('sendMail', err.message));
+}
